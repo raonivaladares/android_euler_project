437480dc76d71c2a75c4a7adcb9dcaeb2344e815
rov <valadares.thel@gmail.com>
2016-12-13 18:46:33 +0100

    Adds ProblemDetailsActivity

--- app/src/main/java/com/developers2/eulerproject/MainActivity.java
+++ app/src/main/java/com/developers2/eulerproject/MainActivity.java
@@ -1,7 +1,15 @@
 package com.developers2.eulerproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ListView listView = (ListView) findViewById(R.id.listView);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listContent());
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position,
+                                    long id) {
+                Intent intent = new Intent(MainActivity.this, ProblemDetailsActivity.class);
+                int eulerProblem = position;
+                intent.putExtra("aaa", eulerProblem);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private List<String> listContent() {
+        List<String> list = new  ArrayList<>();
+        for (int count = 1; count <= 1; count++ ) {
+            list.add("Euler " + count );
+        }
+        return list;
     }
 }
